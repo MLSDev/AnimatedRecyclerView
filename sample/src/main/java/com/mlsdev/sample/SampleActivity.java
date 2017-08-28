6f425925f775ff8792c02a4d483ce43bbc1cc0af
Serhii Petrosyuk <petrosyuk@mlsdev.com>
2017-08-28 17:33:39 +0300

    Init commit

---
+++ sample/src/main/java/com/mlsdev/sample/SampleActivity.java
@@ -0,0 +1,15 @@
+package com.mlsdev.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mlsdev.animatedrv.R;
+
+public class SampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+    }
+}
